61f180ec5a971b1c07cabfee2cb3498a9a3507b8
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-07-30 22:22:34 +0300

    13.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const tokenExtractor = require('../middlewares/tokenExtractor')
 const { Blog, User } = require('../models')
+const { sequelize } = require('../util/db')
 require('express-async-errors')
 const blogFinder = async (req, res, next) => {
     req.blog = await Blog.findByPk(req.params.id)
@@ -8,12 +9,16 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
+    let title = req.query.title ? req.query.title.toLowerCase() : ''
     const blogs = await Blog.findAll({
         attributes: {
             exclude: ['userId']
         },
         include: {
             model: User, attributes: ['name']
+        },
+        where: {
+            title: sequelize.where(sequelize.fn('LOWER', sequelize.col('title')), 'LIKE', '%' + title + '%')
         }
     })
     res.json(blogs)
@@ -25,7 +30,7 @@ router.post('/', tokenExtractor, async (req, res) => {
     res.json(blog)
 })
 
-router.delete('/:id',tokenExtractor, async (req, res) => {
+router.delete('/:id', tokenExtractor, async (req, res) => {
     const user = await User.findByPk(req.decodedToken.id)
     let blog = await Blog.findByPk(req.params.id)
     if (blog.userId !== user.id) {
