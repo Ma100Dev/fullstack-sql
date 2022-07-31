c8c37007c0b0aeced9989656339a10c6f0807f77
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-07-31 11:11:24 +0300

    13.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -31,7 +31,10 @@ router.get('/', async (req, res) => {
                     }
                 }
             ]
-        }
+        },
+        order: [
+            ['likes', 'DESC']
+        ]
     })
     res.json(blogs)
 })
