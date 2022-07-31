786c1030f10edc23632cf28cc2cffebbb7247d07
Ma100Dev <33522543+Ma100Dev@users.noreply.github.com>
2022-07-31 21:05:10 +0300

    13.22

--- controllers/readinglist.js
+++ controllers/readinglist.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 require('express-async-errors')
-const { ReadingListContent, ReadingList } = require('../models')
+const tokenExtractor = require('../middlewares/tokenExtractor')
+const { ReadingListContent, ReadingList, User } = require('../models')
 
 router.post('/', async (req, res) => {
     const { userId, blogId } = req.body
@@ -13,5 +14,27 @@ router.post('/', async (req, res) => {
     const content = await ReadingListContent.create({ readingListId: readingListId.dataValues.id, blogId })
     res.json(content)
 })
+
+router.put('/:id',tokenExtractor, async (req, res) => {
+    const user = await User.findByPk(req.decodedToken.id)
+    const content = await ReadingListContent.findByPk(req.params.id)
+    const readingListId = await ReadingList.findOne({
+        where: {
+            userId: user.id
+        }
+    })
+    if (content.readingListId !== readingListId.dataValues.id) {
+        res.status(403).end()
+    }
+    const updatedContent = await ReadingListContent.update({
+        read: req.body.read
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+    res.json(updatedContent)
+})
+
 
 module.exports = router
